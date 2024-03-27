7509641111061090a188c5abdfb0e6cd49ff61d5
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-03-28 00:47:18 +0600

    SSG By Next-Level Data Fetching

--- src/app/page.js
+++ src/app/page.js
@@ -4,22 +4,28 @@ import Counter from "@/component/Counter";
 
 
 const HomePage =async () => {
-  const res=await fetch("http://localhost:5000/shoes");
+  const res=await fetch("http://localhost:5000/shoes",{
+    cache:"force-cache"
+  });
   const shoes= await res.json();
   console.log(shoes)
   return (
     <div className="">
-      <h1 className="text-center">Welcome to nextJS App: Home page</h1>
+      <h1 className="text-center">Welcome to nextJS App: Home page updated</h1>
       <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-2 mx-auto">
         {
           shoes.slice(0,6).map((shoe)=>
-          <div key={shoe.id} className="card w-80 bg-base-100 shadow-xl mx-auto">
+          <div key={shoe.id} className="card w-80 mx-auto bg-base-100 shadow-xl">
   <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-  <div className="card-body">
-    <h2 className="card-title">{shoe.title}</h2>
-    <p>{shoe.description}</p>
+  <div className="card-body ">
+    <h2 className="card-title text-pink-400">
+      {shoe.title}
+      <div className="badge badge-secondary">${shoe.price}</div>
+    </h2>
+    <p className="text-pink-400">{shoe.description}</p>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+    <button className="btn btn-outline btn-secondary">Buy Now</button>
+    <button className="btn btn-outline btn-secondary">Details</button>
     </div>
   </div>
 </div>
